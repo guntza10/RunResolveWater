c58f75098731b4a9d4c6f4ba3d2b29f101b77053
jomphop <jomphop@perfenterprise.com>
2020-06-01 12:39:16 +0700

    implement insert data watersource from csv

---
+++ Test/models/WaterSourceData.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test.models
+{
+    public class WaterSourceData
+    {
+        // EA
+        public string EA { get; set; }
+        public double WaterSources { get; set; }
+    }
+}
